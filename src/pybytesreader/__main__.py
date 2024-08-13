2cff2553a5699418a821a14e4bb131eb0f29cd30
Paolo Guglielmino <paolo.guglielmino@popso.it>
2024-08-13 16:02:30 +0200

    Better input parameters settings

--- src/pybytesreader/__main__.py
+++ src/pybytesreader/__main__.py
@@ -5,10 +5,10 @@ from pybytesreader.modules.core_functions import read_file_from_line_byte_by_byt
 def main() -> None:
 
     # Example of use
-    input_file = 'examples/data/example_dataset.txt'
-    output_file = 'examples/data/output_file.txt'
-    start_line: int = 3  # Start reading from line 3
-    number_of_bytes: int = 5 # Read the first 5 bytes of the start line
+    input_file = input('Input file path: ')
+    output_file = input('Output file name: ')
+    start_line: int = int(input('Start reading from line number: '))
+    number_of_bytes: int = int(input('Read this number of bytes: '))
 
     rfflbbb(input_file, output_file, start_line, number_of_bytes)
 
