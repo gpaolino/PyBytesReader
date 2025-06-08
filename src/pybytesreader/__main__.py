fb1384a6dff1c7742dd9560804f031bb1493d987
gpaolino <gp.guglielminopaolo@me.com>
2025-06-08 23:32:21 +0200

    Code refactor, parsing parameters in main file.

--- src/pybytesreader/__main__.py
+++ src/pybytesreader/__main__.py
@@ -1,4 +1,4 @@
-#!.venv\Scripts\python.exe
+import argparse
 
 try:
     import pybytesreader.modules.core_functions as cf
@@ -7,15 +7,29 @@ except ImportError as e:
     logging.log(logging.ERROR, "Please run 'pip install -r requirements.txt'")
     raise e
 
+
+parser = argparse.ArgumentParser(description = 'Simple file reader in Python3.')
+
+__VERSION__ = cf.get_version()
+parser.add_argument('-v', '--version', action = 'version', version = __VERSION__)
+
+parser.add_argument('input_file', metavar = 'input_file', type = str, help = 'Input file path')
+parser.add_argument('output_file', metavar = 'output_file', type = str, help = 'Output file name')
+
+
 def main() -> None:
 
-    # Example of usage
-    #input_file = input('Input file path: ')
-    #output_file = input('Output file name: ')
+    # Parse input parameters
+    args = parser.parse_args()
+    input_file = args.input_file
+    output_file = args.output_file
+
+    # Ask for user input
     start_line: int = int(input('Start reading from line number: '))
     number_of_bytes: int = int(input('Read this number of bytes: '))
 
-    cf.read_file_from_line_byte_by_byte(start_line, number_of_bytes)
+    # Call subroutine
+    cf.read_file_from_line_byte_by_byte(input_file, output_file, start_line, number_of_bytes)
 
     print('Press any key to exit...')
     input()
--- src/pybytesreader/modules/core_functions.py
+++ src/pybytesreader/modules/core_functions.py
@@ -1,25 +1,13 @@
-import argparse
 import logging
 import os.path
 
 import pkg_resources
 
-parser = argparse.ArgumentParser(description = 'Simple file reader in Python3.')
 
 def get_version() -> str:
     version = pkg_resources.require('pybytesreader')[0].version
     project_name = pkg_resources.require('pybytesreader')[0].project_name
     return f'{project_name}, version {version}'
-
-__VERSION__ = get_version()
-parser.add_argument('-v', '--version', action = 'version', version = __VERSION__)
-
-parser.add_argument('input_file', metavar = 'input_file', type = str, help = 'Input file path')
-parser.add_argument('output_file', metavar = 'output_file', type = str, help = 'Output file name')
-args = parser.parse_args()
-
-input_file = args.input_file
-output_file = args.output_file
 
 def set_output_directory() -> str:
     output_dir = './output/'
@@ -27,7 +15,7 @@ def set_output_directory() -> str:
         os.makedirs(output_dir)
     return output_dir
 
-def read_file_from_line_byte_by_byte(start_line, number_of_bytes) -> None:
+def read_file_from_line_byte_by_byte(input_file, output_file, start_line, number_of_bytes) -> None:
     # Set default output directory
     output_file_path = set_output_directory() + output_file
 
